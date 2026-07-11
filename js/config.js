a36b27d99c7bbe5d2ece2fb8c448f47b90a3f140
Ahmad Adel Elzek <92895613+ahmaddadel@users.noreply.github.com>
2026-07-11 14:07:03 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Wedding location configuration
- */
-const WEDDING_LOCATION = {
-  mapsUrl: 'https://maps.app.goo.gl/Y4a3c31okeoMthuk6',
-  lat: 30.657243,
-  lng: 31.579922,
-  embedUrl: 'https://maps.google.com/maps?q=30.657243,31.579922&z=16&output=embed'
-};
